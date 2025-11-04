45eafc9556995168a49023aea20e9b17badf161b
Aotow6 <139724883+Aotow6@users.noreply.github.com>
2025-11-04 21:30:07 +0800

    Update Main.java

--- src/view/Main.java
+++ src/view/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package view;
 import com.google.api.client.json.gson.GsonFactory;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -14,7 +14,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.SessionFactory;
 import util.*;
-import view.*;
+import model.*;
 
 /**
  *
